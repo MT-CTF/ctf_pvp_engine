4abfda056f77a03b4dae568b623f8ca03f3231eb
rubenwardy <rubenwardy@gmail.com>
2017-10-10 22:57:33 +0100

    Fix typo

--- ctf_chat/init.lua
+++ ctf_chat/init.lua
@@ -40,7 +40,7 @@ minetest.register_chatcommand("team", {
 		local create = string.match(param, "^add ([%a%d_-]+)")
 		local remove = string.match(param, "^remove ([%a%d_-]+)")
 		local lock = string.match(param, "^lock ([%a%d_-]+)")
-		local lock = string.match(param, "^unlock ([%a%d_-]+)")
+		local unlock = string.match(param, "^unlock ([%a%d_-]+)")
 		local j_name, j_tname = string.match(param, "^join ([%a%d_-]+) ([%a%d_]+)")
 		local b_tname, b_pattern = string.match(param, "^bjoin ([%a%d_-]+) ([%a%d_-%*%! ]+)")
 		local l_name = string.match(param, "^removeplr ([%a%d_-]+)")
